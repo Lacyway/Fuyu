8673e7dc212b07a66a71b9db6924764825a4db4a
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-01-11 15:05:39 +0100

    Add timeservice

---
+++ Fuyu.Backend.BSG/Services/TimeService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Fuyu.Backend.BSG.Services;
+
+public class TimeService
+{
+    public static TimeService Instance => instance.Value;
+    private static readonly Lazy<TimeService> instance = new(() => new TimeService());
+
+    private TimeService()
+    {
+
+    }
+
+    /// <summary>
+    /// Returns a timestamp in seconds
+    /// </summary>
+    public int Timestamp
+    {
+        get
+        {
+            return (int)(TimestampMs / 1000);
+        }
+    }
+
+    /// <summary>
+    /// Returns a timestamp in milliseconds
+    /// </summary>
+    public long TimestampMs
+    {
+        get
+        {
+            return DateTimeOffset.Now.ToUniversalTime().ToUnixTimeMilliseconds();
+        }
+    }
+}
